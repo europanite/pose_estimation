20846a4feddfbfb56b2f9375a166a71f2e01e02c
europanite <europanite@gmail.com>
2026-07-28 00:43:43 +0900

    update/libs

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -4,6 +4,10 @@ import io
 import json
 
 import pytest
+from fastapi import HTTPException
+from fastapi.testclient import TestClient
+from starlette.datastructures import UploadFile
+
 from app import (
     BODY25_TO_MEDIAPIPE,
     _estimate_pose,
@@ -11,9 +15,6 @@ from app import (
     _read_image,
 )
 from app import app as fastapi_app
-from fastapi import HTTPException
-from fastapi.testclient import TestClient
-from starlette.datastructures import UploadFile
 
 client = TestClient(fastapi_app)
 
